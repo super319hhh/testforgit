7cf52bfc92352bf1a685999142d305616edbce9f
bomesh <bo@meshmd.com>
2020-04-29 21:12:23 -0400

    sfsaaaaa
    bingbingn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 import {init} from './utils/api'
-import.artre
-import './index.css'
+import.artre\\screenxxs
 impfvbgyvygdnd/daw
 if (window.Cypress) {
   init({baseURL: window.Cypress.env('API_URL').trim()})
